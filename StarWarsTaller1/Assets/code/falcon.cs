fafb37c2b28ea1a4d4921596027176cc94707639
mdleiton <mau_leiton96@hotmail.com>
2019-06-09 13:01:04 -0500

    [add] se mueve nave

---
+++ StarWarsTaller1/Assets/code/falcon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class falcon : MonoBehaviour{
+
+    public Rigidbody2D body;
+    public float speed = 10f;
+    public meteorit meteorit;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        var x = Input.GetAxis("Horizontal"); 
+        var y = Input.GetAxis("Vertical"); 
+    	body.velocity =  new Vector2(speed*x,speed *y);
+        /*
+        if (Input.GetKey(KeyCode.Space)) {
+         Instantiate(meteorit);
+         Debug.Log("create");
+
+        }*/
+    }
+}
